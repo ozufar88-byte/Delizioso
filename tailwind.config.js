f310c2bf219b8f17147b23f3e6d6ab7dee6789fa
Abdulaziz <sadullayevabdulaziz2009@gmail.com>
2026-02-26 12:15:06 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primery: '#FF8A00',
+        secondary: '#3FA72F',
+        thirdstage: '#311F09'
+      }
+    },
+  },
+  plugins: [],
+}
+
